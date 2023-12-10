786a1e3ab505b0af77940664943140a6e6b8b87b
derrauter <dev@derrauter.de>
2023-12-10 14:18:05 +0100

    Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,36 +2,47 @@ import java.io.File
 import kotlin.math.pow
 
 fun main() {
-
+fun part1() {
     File("src/Day04.txt").readLines()
-        .map{line ->
-           val(_,winnings,havings) = line.split(":","|")
-           val wNumbers =  winnings.split(" ").filter{it.isNotBlank()}.map{it.toInt()}
-           val hNumbers =  havings.split(" ").filter{it.isNotBlank()}.map{it.toInt()}
+        .map { line ->
+            val (_, winnings, havings) = line.split(":", "|")
+            val wNumbers = winnings.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            val hNumbers = havings.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
             wNumbers to hNumbers
-        }.map {
-            (wNumbers,hNumbers) ->
+        }.map { (wNumbers, hNumbers) ->
             val count = wNumbers.count { it in hNumbers }
             if (count == 0) 0
-            else 2.0.pow(count-1).toInt()
+            else 2.0.pow(count - 1).toInt()
         }
         .onEach(::println)
         .sum()
         .also(::println)
+}
 
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part2() {
+        data class Card(val winnings:List<Int>, val havings:List<Int>)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    File("src/Day04.txt").readLines()
+        .map { line ->
+            val (_, winnings, havings) = line.split(":", "|")
+            val wNumbers = winnings.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            val hNumbers = havings.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            val count = wNumbers.count { it in hNumbers }
+            Card(wNumbers,hNumbers) to count
+        }.let { pairs ->
+            val countCard = MutableList(pairs.size){1}
+            pairs.mapIndexed {index, (_,count) ->
+                (1..count).forEach {
+                    countCard[index + it]+= countCard[index]
+                }
 
-    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day04Test")
-//    check(part1(testInput) == 1)
-//
-//    val input = readInput("Day04.txt")
-//    part1(input).println()
-//    part2(input).println()
+            }
+            countCard
+        }
+        .sum()
+        .also(::println)
+}
+
+    //part1()
+    part2()
 }
